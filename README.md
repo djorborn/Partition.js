43601eff1bcedc4ffb1417628170a065dde6dac7
Daniel Osborn <djorborn@gmail.com>
2018-06-06 17:32:36 -0400

    kinda ready

---
+++ README.md
@@ -0,0 +1,71 @@
+# Partition
+Partition lets you make resizable elements. Pass it two elements that share the same parent element and the direction u want do split it, vertical or horizontal. Thats it.
+
+```js
+var splitScreen = new Partition({
+    direction: 'vertical',
+    a: '#left',
+    b: '#right',
+    barWidth: '8'
+});
+```
+```css
+#left, #right {
+
+}
+```
+To make a new Partition: `new Partition({options})`:
+### Options
+- __direction__
+    - horizontal
+    - vertical
+    - Is Required
+- __a__ and __b__
+    - DOM element, String or DOM-Selector
+    - Is Required
+- __bar__
+    - DOM element, String or DOM-Selector
+    - Will be inserted between a and b if not added.
+- __barWidth__
+    - The thickness of the resize bar in pixels.
+    - Default is 10
+- __stopGap__
+    - Where the the bar will stop in percent.
+    - defualt is 10
+- __splitSize__
+    - The initial size of the elements in percent.
+    - defualt is '50 50'
+
+## Example Code
+HTML
+```html
+<div id="app">
+    <div id="left"></div>
+    <div id="right"></div>
+</div>
+```
+CSS
+```css
+#app {
+    width: 500px;
+    height: 500px;
+    margin: auto;
+}
+#left {
+    background-color: pink;
+}
+#right {
+    background-color: lightblue;
+}
+```
+JS
+```js
+var part = new Partition({
+    direction: 'vertical',
+    a: '#left',
+    b: '#right',
+})
+```
+
+### Style Options
+The bars will have the class name __bar-__ plus the direction like __bar-horizontal__.
---
+++ partition.js
@@ -0,0 +1,160 @@
+/*
+    I need to set the width and height on init.
+    style for a and b
+    vertical -
+        * width: data.split% || 50%;
+        * height: 100%;
+        * float: left;
+    style for bar -
+        * width: barWidth || 10px;
+*/
+
+(function (global, factory) {
+    this.factory = factory()
+}(this, (function () {
+
+    this.Partition = Partition;
+
+    function Partition (/* options */) {
+        var options = arguments[0];
+        var data = {};
+        var handler = {};
+
+        /**
+         * Check for options arguments
+         * make sure it is an Object
+         */
+        if (typeof options !== 'object') {
+            console.error('Please pass options as an Object');
+            return 1;
+        }
+        // Direction
+        data.direction = options.direction || 'horizontal';
+
+        //Set elements a and b.
+        data.a = document.querySelector(options.a);
+        data.b = document.querySelector(options.b);
+
+        data.splitSize = options.splitSize || '50 50';
+        data.splitSize = data.splitSize.split(' ');
+
+        // Set Bar
+        data.barWidth = options.barWidth || 10;
+        data.bar = options.bar || document.createElement('div');
+        // Initial Style For A, B and Bar
+            // Vertical Settings
+        if (data.direction === 'vertical') {
+            // A
+            data.a.style.width = 'calc('+data.splitSize[0]+'% - '+data.barWidth/2+'px)';
+            data.a.style.height = '100%';
+            //B
+            data.b.style.width = 'calc('+data.splitSize[1]+'% - '+data.barWidth/2+'px)';
+            data.b.style.height = '100%';
+            // Bar
+            data.bar.style.width = data.barWidth + 'px';
+            data.bar.style.height = '100%';
+            data.bar.style.cursor = 'ew-resize';
+        } else {
+            // Horizontal Settings
+            // A
+            data.a.style.height = 'calc('+data.splitSize[0]+'% - '+data.barWidth/2+'px)';
+            data.a.style.width = '100%';
+            //B
+            data.b.style.height = 'calc('+data.splitSize[1]+'% - '+data.barWidth/2+'px)';
+            data.b.style.width = '100%';
+            // Bar
+            data.bar.style.height = data.barWidth + 'px';
+            data.bar.style.width = '100%';
+            data.bar.style.cursor = 'ns-resize';
+        }
+        // Global Style Settings
+        data.a.style.float = 'left';
+        data.b.style.float = 'left';
+        data.bar.style.float = 'left';
+
+        // Insert Bar Element
+        data.a.parentNode.insertBefore(data.bar, data.b);
+
+        data.mousedown = false;
+
+
+        handler.get = function(obj, key) {
+            return obj[key];
+        }
+        handler.set = function(obj, key, value) {
+            if (key === 'resize') {
+                // Resize A and B
+                if (obj['direction'] === 'horizontal') {
+                    resizeHorizontal({
+                        y: value.y,
+                        a: obj['a'],
+                        b: obj['b'],
+                        bar: obj['bar'],
+                        barWidth: obj['barWidth']
+                    })
+                } else {
+                    resizeVertical({
+                        x: value.x,
+                        a: obj['a'],
+                        b: obj['b'],
+                        bar: obj['bar'],
+                        barWidth: obj['barWidth']
+                    })
+                }
+            }
+            obj[key] = value;
+        }
+
+        var proxy = new Proxy(data, handler);
+
+        proxy.bar.addEventListener('mousedown', function (event) {
+            proxy.mousedown = true;
+        })
+
+        proxy.a.parentElement.onmousemove = function (e) {
+            if (proxy.mousedown) {
+                var resize = {
+                    x: e.pageX,
+                    y: e.pageY
+                }
+                proxy.resize = resize;
+            }
+        }
+
+        proxy.a.parentNode.onmouseleave = function () {
+            proxy.mousedown = false;
+        }
+
+        proxy.a.parentNode.onmouseup = function () {
+            proxy.mousedown = false;
+        }
+
+        this.fullStop = function () {
+            proxy.mousedown = false;
+        }
+
+        this.mousedown = proxy.mousedown;
+
+        function resizeVertical(obj) {
+            var bcr = obj.a.parentElement.getBoundingClientRect();
+            var offset = bcr.left;
+            var rootWidth = bcr.width;
+            var cursor = obj.x;
+            cursor -= offset;
+            var percent = (cursor/rootWidth)*100;
+            obj.a.style.width = 'calc('+percent+'% - '+(obj.barWidth/2)+'px)';
+            obj.b.style.width = 'calc('+(100 - percent)+'% - '+(obj.barWidth/2)+'px)';
+        }
+        function resizeHorizontal(obj) {
+            var bcr = obj.a.parentElement.getBoundingClientRect();
+            var offset = bcr.top;
+            var rootHeight = bcr.height;
+            var cursor = obj.y;
+            cursor -= offset;
+            var percent = (cursor/rootHeight)*100;
+            obj.a.style.height = 'calc('+percent+'% - '+(obj.barWidth/2)+'px)';
+            obj.b.style.height = 'calc('+(100 - percent)+'% - '+(obj.barWidth/2)+'px)';
+        }
+    }
+
+})))
